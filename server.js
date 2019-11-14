6018ac383576c13dae1306e69ae6529431b7cac3
cucurbitbaby <cucurbitbaby@163.com>
2019-11-15 00:09:10 +0800

    a *4

--- server.js
+++ server.js
@@ -29,15 +29,16 @@ mongoose
 app.use(passport.initialize());
 
 
-//设置允许跨域访问该服务.
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Content-Type', 'application/json;charset=utf-8');
+//设置跨域访问
+app.all('*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
   next();
 });
+
 
 require('./config/passport')(passport);
 
